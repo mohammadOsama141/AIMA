9e447dba7f4edc07f6445bfe842d2d5740567db0
aena800 <anariaz570@gmail.com>
2024-02-29 20:34:23 +0500

    Baseic Navbar and Introductory section of homepage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './Pages/Homepage/Home';
 
 function App() {
   return (
     <>
-    <div className="container">
-      
-
+    <div className="navcontainer">
+      <NavBar/>
+    </div>
+    <div>
+      <Home></Home>
     </div>
     </>
   );
